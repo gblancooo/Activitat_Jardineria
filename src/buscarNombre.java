d7c28febbe3a6aace4e26cf607f6d3985cc043ef
gblancooo <gerardblanco2006@gmail.com>
2025-02-06 16:30:13 +0100

    Lista + scanner
    Insertar el scanner y hacer que salga la lista

--- src/buscarNombre.java
+++ src/buscarNombre.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class buscarNombre {
-
+	Scanner scan_numeros = new Scanner(System.in);
+	Scanner scan_letras = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("BUSCAR POR NOMBRE DE PLANTA");
+		System.out.println("LISTA");
+		for (int i = 0; i < main.hierba.length; i++) {
+			System.out.println(main.hierba[i]);
+		
+		}
 
 	}
 
