fd8354e51f3b0ca4d8bba70cad441111bb76ac5e
gblancooo <gerardblanco2006@gmail.com>
2025-02-07 18:47:53 +0100

    Pedir origen y mostrar lista

--- src/buscarOrigen.java
+++ src/buscarOrigen.java
@@ -1,9 +1,29 @@
+import java.util.Scanner;
 
 public class buscarOrigen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan_numeros = new Scanner(System.in);
+		Scanner scan_letras = new Scanner(System.in);
+	
+		String origen = "";
+		int parar = 1;
+		
+		System.out.println("BUSCAR POR ORIGEN DE PLANTA");
+		System.out.println("LISTA");
+		for (int i = 0; i < main.origen.length; i++) {
+			System.out.println(main.origen[i]);
+		
+		}
+		System.out.println("Que planta quieres?");
+		origen = scan_letras.nextLine();
+		
+		
+		
+		
 	}
 
+	
+	
 }
