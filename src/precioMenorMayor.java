6d88881873cf52c78845a33679abc5769d57bed3
gblancooo <gerardblanco2006@gmail.com>
2025-02-07 19:41:41 +0100

    plantas + imprimir
    Ordenar el nombre de las plantas con el precio que le corresponde a cada una para que el usuario tenga una mejor claridad de los precios

--- src/precioMenorMayor.java
+++ src/precioMenorMayor.java
@@ -6,23 +6,40 @@ public class precioMenorMayor {
 
 	public static void main(String[] args) {
 
+		//precios
 		double[] precios = new double[main.precio.length];
-		
 		for (int i = 0; i < precios.length; i++) {
 			precios[i] = main.precio[i];
 		}
+				
+		Arrays.sort(precios);	// Arrays.sort() ordena el array de menor a mayor de forma automática
+				
+		
+		//plantas
+		String[] hierba = new String[main.hierba.length];
+				
+		for (int i = 0; i < hierba.length; i++) {
+			for (int j = 0; j < hierba.length; j++) {
+				if(precios[i] == main.precio[j]) {
+					hierba[i] = main.hierba[j];
+				}
+			}
+		}
+		
+		
 		
 		System.out.println();
 		System.out.println();
 		System.out.println("El precio de menor a mayor:");
 				
-		Arrays.sort(precios);	// Arrays.sort() ordena el array de menor a mayor de forma automática
+		
+		
+
 
 		
 		for (int i = 0; i < precios.length; i++) {
-			System.out.println(precios[i]);	
+			System.out.println(precios[i] + " -- " + hierba[i]);	
 		}
-		
-	}
 
+	}	
 }
