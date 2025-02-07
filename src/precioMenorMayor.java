ea37cf3661db0ecef0de19b941bda1fbbed334c9
gblancooo <gerardblanco2006@gmail.com>
2025-02-07 19:18:10 +0100

    ordenador precios + imprimir
    Ordeno el array de menor a mayor con el array.sort y luego imprimo la lista de precios ordenada

--- src/precioMenorMayor.java
+++ src/precioMenorMayor.java
@@ -1,9 +1,28 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class precioMenorMayor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		double[] precios = new double[main.precio.length];
+		
+		for (int i = 0; i < precios.length; i++) {
+			precios[i] = main.precio[i];
+		}
+		
+		System.out.println();
+		System.out.println();
+		System.out.println("El precio de menor a mayor:");
+				
+		Arrays.sort(precios);	// Arrays.sort() ordena el array de menor a mayor de forma automática
+
+		
+		for (int i = 0; i < precios.length; i++) {
+			System.out.println(precios[i]);	
+		}
+		
 	}
 
 }
