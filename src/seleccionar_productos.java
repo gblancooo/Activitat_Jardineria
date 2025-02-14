ec9a4a21c15d827cef22665703abaaa54ec00409
gblancooo <gerardblanco2006@gmail.com>
2025-02-14 19:22:47 +0100

    pedir cantidad de productos

--- src/seleccionar_productos.java
+++ src/seleccionar_productos.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 
 public class seleccionar_productos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan_letras = new Scanner(System.in);
+		
+		int cantidad = 0;
+		
+		System.out.println("Cuantos productos quieres seleccionar?");
+		cantidad = scan_letras.nextInt();
+		String[] productos = new String[cantidad];
+		
 	}
 
 }
